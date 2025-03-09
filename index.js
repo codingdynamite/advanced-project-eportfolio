45a695321e0ca702a21e260122ee1aa28d04e907
Joshua Mason <thelementalcode@gmail.com>
2025-03-09 13:13:08 -0400

    dark mode complete

---
+++ index.js
@@ -0,0 +1,44 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += ' dark-theme'
+    }
+    else {
+        document.body.classList.remove('dark-theme');
+    }
+    
+}
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += ' modal__overlay--visible';
+    emailjs
+        .sendForm(
+          'service_7zgo0un',
+          'template_xe6eb3x',
+          event.target,
+          'W_lgO3TBjTNfdLdEf'
+    )  .then(() => {
+        loading.classList.remove('modal__overlay--visible');
+        success.classList += ' modal__overlay--visible';
+    }).catch(() => {
+        loading.classList.remove('modal__overlay--visible');
+        alert(
+            'the email service is temporarily unavailable. Please contact me directly on email@email.com'
+        );
+     })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove('modal--open');
+    }
+    isModalOpen = true;
+    document.body.classList += ' modal--open';
+}
